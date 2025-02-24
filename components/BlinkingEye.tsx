eb4f2336a51391c72426230720517bdb71f87fcc
Drew <milbrathdrew@gmail.com>
2025-02-24 15:40:51 -0700

    added blinking eye code

---
+++ components/BlinkingEye.tsx
@@ -0,0 +1,10 @@
+// components/BlinkingEye.tsx
+export default function BlinkingEye() {
+  return (
+    <div className="eye-container my-8">
+      <div className="eye">
+        <div className="pupil"></div>
+      </div>
+    </div>
+  )
+}
